2c300416f659db970a3ac4efad66da43b336e682
INTEL <INTEL@INTEL-PC>
2020-04-28 20:23:10 +0530

    SeleniumTest project pushed to Github respository

---
+++ SeleniunTest/src/FirstClass.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		//System.setProperty("webdriver.chrome.driver", "D:\\Selenium Jars\\chromedriver.exe");
+	System.setProperty("webdriver.gecko.driver", "E:\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		//WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.youtube.com");
+		
+		System.out.println("Youtube is launched");
+		//driver.close();
+		
+	}
+
+}
